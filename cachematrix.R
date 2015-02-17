4141de1463e79c4153ebe5dfd833dc4d92c8911e
Hua Wu <hua.wu02@gmail.com>
2015-02-16 17:50:57 -0800

    added function body in makeCacheMatrix, cacheSolve
    refer to comments for example function calls

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## example script to cache the inverse of matrics by using makeCacheMatrix 
+## and cachSolve:
 
-## Write a short comment describing this function
+## > source('cachematrix.R')
+## > a <- matrix(c(1,2,3,5), 2, 2)
+## > ma <- makeCacheMatrix(a)
+## > cacheSolve(ma)
+##      [,1] [,2]
+## [1,]   -5    3
+## [2,]    2   -1
+## > cacheSolve(ma)
+## getting cached data
+##      [,1] [,2]
+## [1,]   -5    3
+## [2,]    2   -1
 
+
+## create a matrix with its inversion saved if it exists
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)    
 }
 
-
-## Write a short comment describing this function
-
+## solve the inversion of a matrix, search for the already existing inversion first
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    mtx <- x$get()
+    inv <- solve(mtx, ...)
+    x$setinv(inv)
+    inv 
 }
